caf4ff539d0440691d64d75cab92221ebe57388f
Victor Dutra <domagal@gmail.com>
2021-11-06 13:45:09 -0300

    Routes

---
+++ frontend/src/dist/App.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var Routes_1 = require("Routes");
+function App() {
+    return (React.createElement(Routes_1["default"], null));
+}
+exports["default"] = App;
